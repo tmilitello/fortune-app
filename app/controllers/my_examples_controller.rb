95999d0819487039cd93a0c521bfdc7fb3a5145e
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-05-02 13:31:40 -0500

    adds app update

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,6 +1,39 @@
 class MyExamplesController < ApplicationController
+  times_visited = 0
   def random_fortune
     number = rand(1..10000)
-    puts "You will die in #{number} days!"
+    #render json: {message: "hello"}
+
+    #render json: {message: "You will die in...", days: number}
+    render json: {message: "You will die in...#{number}", days: number}
+    #"<h1>You will die in #{number} days!</h1>".html_safe
   end
+
+  def lotto_numbers
+    lotto_array = []
+    6.times do
+    lotto_array << rand(1..60)
+    end
+    render json: {message: lotto_array}
+  end
+
+
+def bottles_lyrics
+  bottles = 100
+  index = 0
+  song_array = []
+  while bottles > 0
+  
+    bottles << "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer!
+    Take one down, Pass it around, #{bottles - 1} bottles of beer on the wall!"
+   bottles -= 1
+  end
+  render json: {message: song_array}
+ end
 end
+
+
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   
-  get "/random_fortune", controller: "example_pages", action: "random_fortune"
+  get "/random_fortune", controller: "my_examples", action: "random_fortune"
+  get "/lotto_numbers", controller: "my_examples", action: "lotto_numbers"
+  get"/bottles_lyrics", controller: "my_examples", action: "bottles_lyrics"
 end
